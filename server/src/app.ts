0f0ea95e26d7887645fcfcaf940eab4b0dce179d
Adel Guitoun <adeelguitoun@gmail.com>
2024-06-27 07:22:28 -0700

    Integrate Auth0 for authentication (#2)

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,15 +1,13 @@
 import express, { Request, Response } from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
+import { auth } from 'express-openid-connect';
+import { config } from './middlewares/auth0/config';
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(morgan('dev'));
-
-// Test Route
-app.get('/test', async (req: Request, res: Response) => {
-  res.status(200).json({ message: 'Route successfully accessed' });
-});
+app.use(auth(config));
 
 export default app;
