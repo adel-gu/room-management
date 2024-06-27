d41297e8dd9c3e107f92bbaea003b5f76678bdc9
adel <adeelguitoun@gmail.com>
2024-06-27 15:21:30 +0100

    Integrate Auth0 for authentication

---
+++ server/src/middlewares/auth0/config.ts
@@ -0,0 +1,10 @@
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: process.env.AUTH0_SECRET,
+  baseURL: process.env.BASE_URL,
+  clientID: process.env.AUTH0_CLIENT_ID,
+  issuerBaseURL: process.env.AUTH0_BASE_URL,
+};
+
+export { config };
