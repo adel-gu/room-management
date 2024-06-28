a5b49b50d95f2c3aab082ab96f106e633652ec13
Adel Guitoun <adeelguitoun@gmail.com>
2024-06-28 03:49:53 -0700

    Add global styling (#4)

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,10 +4,14 @@ import App from './App.tsx';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { BrowserRouter } from 'react-router-dom';
 
+// Styles
+import GlobalStyles from './styles/index.ts';
+
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <GlobalStyles />
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
         <App />
