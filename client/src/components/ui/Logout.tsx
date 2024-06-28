5cf6f436b5fec1c0f92557fa13fbfa9a4c8ce5f1
adel <adeelguitoun@gmail.com>
2024-06-28 18:06:44 +0100

    Add logout button

---
+++ client/src/components/ui/Logout.tsx
@@ -0,0 +1,11 @@
+import { LogOut } from 'lucide-react';
+import ButtonIcon from './ButtonIcon';
+
+const Logout = () => {
+  return (
+    <ButtonIcon>
+      <LogOut />
+    </ButtonIcon>
+  );
+};
+export default Logout;
