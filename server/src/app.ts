0eb5d5b39785a96c1e9bfc9894c6cc6528478668
adel <adeelguitoun@gmail.com>
2024-06-29 11:16:49 +0100

    Use the appApi router

--- server/src/app.ts
+++ server/src/app.ts
@@ -4,10 +4,15 @@ import morgan from 'morgan';
 import { auth } from 'express-openid-connect';
 import { config } from './middlewares/auth0/config';
 
+// Routers
+import appApi from './routes/appApi';
+
 const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(morgan('dev'));
 app.use(auth(config));
 
+// App API
+app.use('/api/v1/', appApi);
 export default app;
