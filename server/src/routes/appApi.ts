f1de368050d8d713656ad54faf1b31ae94b2368c
Adel Guitoun <adeelguitoun@gmail.com>
2024-06-30 08:26:51 -0700

    Implement image file uploading for room model (#13)

--- server/src/routes/appApi.ts
+++ server/src/routes/appApi.ts
@@ -1,10 +1,23 @@
 import express from 'express';
+import multer from 'multer';
 import roomController from '../controllers/room';
 
 const router = express.Router();
 
+// Config Multer
+const storage = multer.memoryStorage();
+const upload = multer({
+  storage,
+  limits: {
+    fileSize: 5 * 1024 * 1024, // 5mb
+  },
+});
+
 // Rooms
-router.route('/rooms').get(roomController.readAll).post(roomController.create);
+router
+  .route('/rooms')
+  .get(roomController.readAll)
+  .post(upload.single('roomImage'), roomController.create);
 router
   .route('/rooms/:id')
   .get(roomController.read)
