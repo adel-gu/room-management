b191108b73a4222d94e576304da33eef7162afdf
adel <adeelguitoun@gmail.com>
2024-06-30 15:42:59 +0100

    Add tanstack/query dev tool

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { BrowserRouter } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+import App from './App.tsx';
 
 // Styles
 import GlobalStyles from './styles/index.ts';
 import { Toaster } from 'react-hot-toast';
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: { queries: { staleTime: 0 } },
+});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
@@ -18,6 +22,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
         <App />
         <Toaster position="top-right" reverseOrder={false} />
       </BrowserRouter>
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </React.StrictMode>,
 );
