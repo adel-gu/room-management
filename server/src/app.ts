6bf9e97799e04669f502bb660f713f9afe1467f0
adel <adeelguitoun@gmail.com>
2024-06-30 16:25:21 +0100

    Implement image file uploading for room model

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
+import { v2 as cloudinary } from 'cloudinary';
 import { auth } from 'express-openid-connect';
 import { config } from './middlewares/auth0/config';
 
@@ -12,6 +13,13 @@ app.use(express.json());
 app.use(cors());
 app.use(morgan('dev'));
 app.use(auth(config));
+
+// Config Cloudinary
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 // App API
 app.use('/api/v1/', appApi);
--- server/src/controllers/crud/create.ts
+++ server/src/controllers/crud/create.ts
@@ -1,10 +1,24 @@
 import { Request, Response } from 'express';
 import mongoose from 'mongoose';
+import cloudinary from 'cloudinary';
+import { ModelsEnum } from '../../utils/constants';
 
 const createDoc = (model: string) => async (req: Request, res: Response) => {
   try {
     const Model = mongoose.model(model);
-    const doc = await Model.create(req.body);
+    const doc = new Model(req.body);
+
+    if (model === ModelsEnum.Room) {
+      // Upload room image file
+      const file = req.file as Express.Multer.File;
+      const base64Image = Buffer.from(file.buffer).toString('base64');
+      const dataUrl = `data:${file.mimetype};base64,${base64Image}`;
+
+      const uploadResponse = await cloudinary.v2.uploader.upload(dataUrl);
+      doc.image = uploadResponse.url;
+    }
+
+    await doc.save();
 
     if (!doc)
       return res
