d23b801a4f575296f6b69ca8642ecda2e7032beb
adel <adeelguitoun@gmail.com>
2024-07-01 21:43:03 +0100

    Update spinner

--- client/src/components/Spinner.tsx
+++ client/src/components/Spinner.tsx
@@ -1,10 +1,26 @@
-import { MoonLoader } from 'react-spinners';
+import { Oval } from 'react-loader-spinner';
 import styled from 'styled-components';
 
-const StyledSpinner = styled(MoonLoader)`
+interface Props {
+  size?: number;
+  color?: string;
+}
+
+const StyledSpinner = styled.div`
   margin-inline: auto;
 `;
-const Spinner = () => {
-  return <StyledSpinner size={50} color="#4f46e5" speedMultiplier={0.5} />;
+const Spinner = ({ size = 50, color = '#4f46e5' }: Props) => {
+  return (
+    <StyledSpinner>
+      <Oval
+        visible={true}
+        height={size}
+        width={size}
+        color={color}
+        secondaryColor={color}
+        strokeWidth={5}
+      />
+    </StyledSpinner>
+  );
 };
 export default Spinner;
