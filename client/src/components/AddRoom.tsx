79e03e821a42bfacf0e079845a0fcc750c24d051
adel <adeelguitoun@gmail.com>
2024-07-01 14:34:57 +0100

    Implement AddRoom component trigger

--- client/src/components/AddRoom.tsx
+++ client/src/components/AddRoom.tsx
@@ -1,4 +1,14 @@
+import { useState } from 'react';
+import RoomForm from './RoomForm';
+import Button from './ui/Button';
+
 const AddRoom = () => {
-  return <div>AddRoom</div>;
+  const [isOpen, setIsOpen] = useState(true);
+  return (
+    <div>
+      <Button onClick={() => setIsOpen(true)}>Add new room</Button>
+      <div>{isOpen && <RoomForm />}</div>
+    </div>
+  );
 };
 export default AddRoom;
