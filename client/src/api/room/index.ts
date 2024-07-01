345e5fff6ffc19d30d6f6d644b9ba6c3d775614c
adel <adeelguitoun@gmail.com>
2024-07-01 15:32:41 +0100

    Implement create new room api request

--- client/src/api/room/index.ts
+++ client/src/api/room/index.ts
@@ -2,10 +2,22 @@ import { Room } from '../../types/room';
 
 const API_BASE_URL = `${import.meta.env.VITE_API_BASE_URL}/rooms`;
 
-export const readAllRooms = async (): Promise<Room[]> => {
+export const readAllRoomsRequest = async (): Promise<Room[]> => {
   const res = await fetch(API_BASE_URL);
   if (!res.ok) throw new Error('Error Fetching rooms!');
   const { data } = await res.json();
 
+  return data;
+};
+
+export const createNewRoomRequest = async (room: FormData) => {
+  const res = await fetch(API_BASE_URL, {
+    method: 'POST',
+    body: room,
+  });
+
+  if (!res.ok) throw new Error('Failed to create new room');
+
+  const { data } = await res.json();
   return data;
 };
