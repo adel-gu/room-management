e270d3f1830b6bb1bdc800c086fee0776d08dab3
adel <adeelguitoun@gmail.com>
2024-07-01 14:31:41 +0100

    Create TextArea UI component

---
+++ client/src/components/ui/Textarea.tsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const Textarea = styled.textarea`
+  border: 1px solid var(--color-grey-300);
+  border-radius: 5px;
+  background-color: var(--color-grey-0);
+
+  padding: 0.8rem 1.2rem;
+  box-shadow: var(--color-grey-0);
+
+  width: 100%;
+`;
+export default Textarea;
