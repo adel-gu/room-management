96afbbf27e2856bb8307cde36706949af758f57e
adel <adeelguitoun@gmail.com>
2024-07-02 19:33:54 +0100

    Update spinner props

--- client/src/components/Spinner.tsx
+++ client/src/components/Spinner.tsx
@@ -2,21 +2,22 @@ import { Oval } from 'react-loader-spinner';
 import styled from 'styled-components';
 
 interface Props {
-  size?: number;
+  size?: string;
   color?: string;
 }
 
 const StyledSpinner = styled.div`
   margin-inline: auto;
 `;
-const Spinner = ({ size = 50, color = '#4f46e5' }: Props) => {
+
+const Spinner = ({ size = 'lg', color = 'primary' }: Props) => {
   return (
     <StyledSpinner>
       <Oval
         visible={true}
-        height={size}
-        width={size}
-        color={color}
+        height={size === 'lg' ? 50 : 20}
+        width={size === 'lg' ? 50 : 20}
+        color={color === 'primary' ? '#4f46e5' : '#f9fafb'}
         secondaryColor={color}
         strokeWidth={5}
       />
