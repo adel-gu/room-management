ece6aab50e4e5cb8402f27675209aa7f188ff69e
adel <adeelguitoun@gmail.com>
2024-07-03 11:33:26 +0100

    Update the update controller handler to accept files uploaded

--- server/src/controllers/crud/update.ts
+++ server/src/controllers/crud/update.ts
@@ -1,13 +1,13 @@
 import { Request, Response } from 'express';
 import mongoose from 'mongoose';
+import { ModelsEnum } from '../../utils/constants';
+import uploadImg from '../../utils/uploadImage';
 
 const updateDoc = (model: string) => async (req: Request, res: Response) => {
   try {
     const Model = mongoose.model(model);
-    const doc = await Model.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true,
-    });
+    // find document
+    const doc = await Model.findById(req.params.id);
 
     if (!doc)
       return res.status(404).json({
@@ -15,8 +15,31 @@ const updateDoc = (model: string) => async (req: Request, res: Response) => {
         message: 'Document with that ID is not found',
       });
 
+    for (const key in req.body) {
+      if (doc.toObject().hasOwnProperty(key)) {
+        doc[key] = req.body[key];
+      }
+    }
+
+    // Validate doc
+    const validationErrors = doc.validateSync();
+    if (validationErrors) {
+      return res.status(400).json({
+        status: 'fail',
+        message: validationErrors,
+      });
+    }
+
+    // Upload the file
+    if (model === ModelsEnum.Room && req.file) {
+      doc.image = await uploadImg(req.file);
+    }
+
+    // Save after validation success
+    await doc.save();
+
     res.status(200).json({
-      status: 'success',
+      message: 'success',
       data: doc,
     });
   } catch (error) {
