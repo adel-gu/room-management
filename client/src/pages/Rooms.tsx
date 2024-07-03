aaf9466a627e0b460c78a4ab15bda61b763d2190
adel <adeelguitoun@gmail.com>
2024-07-03 20:59:23 +0100

    Add QueryOperations to the page

--- client/src/pages/Rooms.tsx
+++ client/src/pages/Rooms.tsx
@@ -1,18 +1,32 @@
 import AddRoom from '../components/AddRoom';
+import QueryOperations from '../components/QueryOperations';
 import RoomsTable from '../components/RoomsTable';
 import Heading from '../components/ui/Heading';
 import Wrapper from '../components/ui/Wrapper';
+import { FilterObjType } from '../types/filter';
+
+const filterObj: FilterObjType = {
+  field: 'discount',
+  options: [
+    { value: 'all', label: 'All' },
+    { value: 'no-discount', label: 'No discount' },
+    { value: 'with-discount', label: 'With discount' },
+  ],
+};
 
 const Rooms = () => {
   return (
     <>
       <Wrapper direction="hr">
         <Heading as="h1">Rooms</Heading>
-        <span>Filter/Sort</span>
+        <QueryOperations filterObj={filterObj} />
       </Wrapper>
       <Wrapper>
         <RoomsTable />
-        <AddRoom />
+        <Wrapper direction="hr">
+          <AddRoom />
+          <div>Pagination</div>
+        </Wrapper>
       </Wrapper>
     </>
   );
