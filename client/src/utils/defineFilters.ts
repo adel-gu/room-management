dc7edfaa981924305ecc2d586abd582b4e95fedd
adel <adeelguitoun@gmail.com>
2024-07-04 12:13:05 +0100

    Update to include the sort param

--- client/src/utils/defineFilters.ts
+++ client/src/utils/defineFilters.ts
@@ -2,13 +2,16 @@ export const defineRoomFilterQuery = (
   field: string | null,
   value: string | null,
   page: string | null,
+  sort: string | null,
 ): string => {
   let query;
 
-  if (value === 'all') query = '';
+  if (value === 'all' || value == null) query = '';
   else query = `?${field}${value}`;
 
   query = query === '' ? `?page=${page ?? 1}` : query + `&page=${page ?? 1}`;
+
+  query = !!sort ? query + `&sort${sort}` : query + `&sort=-createdAt`;
 
   return query;
 };
