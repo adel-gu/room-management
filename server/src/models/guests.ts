038169c0c6856643d8db720780add5f174fb0aa7
adel <adeelguitoun@gmail.com>
2024-07-07 18:23:53 +0100

    Create guests model

---
+++ server/src/models/guests.ts
@@ -0,0 +1,54 @@
+import mongoose, { Model } from 'mongoose';
+import validator from 'validator';
+import { ModelsEnum } from '../utils/constants';
+
+interface IGuest {
+  fullName: string;
+  phone: string;
+  nationality: string;
+  nationalID: string;
+  createdAt: Date;
+  email?: string;
+}
+
+type GuestModelType = Model<IGuest>;
+
+const schema = new mongoose.Schema<IGuest, GuestModelType>({
+  fullName: {
+    type: String,
+    required: [true, 'Guests full name field is required'],
+  },
+  phone: {
+    type: String,
+    required: [true, 'Guest phone field is required'],
+    validate: {
+      validator: function (val: string) {
+        return validator.isMobilePhone(val, 'any');
+      },
+      message: 'Please provide a valid phone number',
+    },
+    trim: true,
+  },
+  nationality: {
+    type: String,
+    required: [true, 'Guests nationality field is required'],
+  },
+  nationalID: {
+    type: String,
+    required: [true, 'Guests national ID field is required'],
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+    immutable: true,
+  },
+  email: {
+    type: String,
+    trim: true,
+    lowercase: true,
+    validate: [validator.isEmail, 'Please provide a correct email'],
+  },
+});
+
+const Guest = mongoose.model<IGuest, GuestModelType>(ModelsEnum.Guest, schema);
+export default Guest;
