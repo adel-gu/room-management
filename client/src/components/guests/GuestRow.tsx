54c1f6b575916cea1198a6883d94e37ee6b8ccc6
adel <adeelguitoun@gmail.com>
2024-07-07 20:59:48 +0100

    Create guest row for the table

---
+++ client/src/components/guests/GuestRow.tsx
@@ -0,0 +1,68 @@
+import { Copy, EllipsisVertical, Pencil, Trash2 } from 'lucide-react';
+import { IGuest } from '../../types/guest';
+import DropdownMenu from '../DropdownMenu';
+import Modal from '../Modal';
+import DeleteAction from '../DeleteAction';
+import styled from 'styled-components';
+
+interface Props {
+  guest: IGuest;
+}
+
+const StyledCell = styled.div`
+  font-size: 1.6rem;
+  font-weight: 600;
+  color: var(--color-grey-600);
+`;
+
+const GuestRow = ({ guest }: Props) => {
+  return (
+    <>
+      <StyledCell>{guest.fullName}</StyledCell>
+      <StyledCell>{guest.phone}</StyledCell>
+      <StyledCell>{guest.email ?? '--'}</StyledCell>
+      <StyledCell>{guest.nationality}</StyledCell>
+      <StyledCell>{guest.nationalID}</StyledCell>
+      <Modal>
+        <DropdownMenu>
+          <DropdownMenu.Trigger>
+            <EllipsisVertical />
+          </DropdownMenu.Trigger>
+          <DropdownMenu.Content>
+            <DropdownMenu.Group>
+              <Modal.Trigger name="edit">
+                <DropdownMenu.Item icon={<Pencil />} disabled={false}>
+                  edit
+                </DropdownMenu.Item>
+              </Modal.Trigger>
+              <DropdownMenu.Item
+                icon={<Copy />}
+                disabled={false}
+                onClick={() => {}}
+              >
+                duplicate
+              </DropdownMenu.Item>
+              <Modal.Trigger name="delete">
+                <DropdownMenu.Item icon={<Trash2 />} disabled={false}>
+                  delete
+                </DropdownMenu.Item>
+              </Modal.Trigger>
+            </DropdownMenu.Group>
+          </DropdownMenu.Content>
+        </DropdownMenu>
+        <Modal.Content name="edit">
+          <span>Form</span>
+          {/* <RoomForm room={room} /> */}
+        </Modal.Content>
+        <Modal.Content name="delete">
+          <DeleteAction
+            resourceName="Guest"
+            handleDeleteAction={() => {}}
+            disabled={false}
+          />
+        </Modal.Content>
+      </Modal>
+    </>
+  );
+};
+export default GuestRow;
