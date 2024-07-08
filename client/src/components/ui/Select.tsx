d641aa1040bfcd8818a0546ae35fea6fb83d3394
adel <adeelguitoun@gmail.com>
2024-07-08 17:22:07 +0100

    Create styled select component

---
+++ client/src/components/ui/Select.tsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const Select = styled.select`
+  border: 1px solid var(--color-grey-300);
+  border-radius: var(--border-radius-sm);
+  background-color: var(--color-grey-0);
+  padding: 0.8rem 1.2rem;
+  box-shadow: var(--shadow-sm);
+  grid-column: 2 / 3;
+  width: 100%;
+`;
+
+export default Select;
