12c938e3b7c7da5e5cff5fc52e5ae0f328bb5735
adel <adeelguitoun@gmail.com>
2024-07-08 12:27:09 +0100

    Apply the guests search query in the api request

--- client/src/api/guests/index.ts
+++ client/src/api/guests/index.ts
@@ -5,7 +5,7 @@ const API_BASE_URL = `${import.meta.env.VITE_API_BASE_URL}/guests`;
 export const readAllGuestsRequest = async (
   query: string,
 ): Promise<GetAllGuestsResType> => {
-  const res = await fetch(`${API_BASE_URL}`);
+  const res = await fetch(`${API_BASE_URL}${query}`);
   if (!res.ok) throw new Error('Error Fetching guests!');
   const { data } = await res.json();
 
