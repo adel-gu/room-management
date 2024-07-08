43bff6b8dc1ebc1f2188802c37c7db3b94173b82
adel <adeelguitoun@gmail.com>
2024-07-08 12:25:34 +0100

    Add search bar component

--- client/src/pages/Guests.tsx
+++ client/src/pages/Guests.tsx
@@ -1,34 +1,15 @@
-import QueryOperations from '../components/QueryOperations';
 import Heading from '../components/ui/Heading';
 import Wrapper from '../components/ui/Wrapper';
-import { FilterObjType, SortByObjType } from '../types/filter';
 import GuestsTable from '../components/guests/GuestsTable';
 import AddGuest from '../components/guests/AddGuest';
-
-// const filterObj: FilterObjType = {
-//   field: 'discount',
-//   options: [
-//     { value: 'all', label: 'All' },
-//     { value: '=0', label: 'No discount' },
-//     { value: '[gt]=0', label: 'With discount' },
-//   ],
-// };
-
-// const sortByObj: SortByObjType = [
-//   { value: '=name', label: 'Sort by name (A-Z)' },
-//   { value: '=-name', label: 'Sort by name (Z-A)' },
-//   { value: '=regularPrice', label: 'Sort by price (asc)' },
-//   { value: '=-regularPrice', label: 'Sort by price (desc)' },
-//   { value: '=maxCapacity', label: 'Sort by capacity (asc)' },
-//   { value: '=-maxCapacity', label: 'Sort by capacity (desc)' },
-// ];
+import SearchBy from '../components/SearchBy';
 
 const Guests = () => {
   return (
     <>
       <Wrapper direction="hr">
         <Heading as="h1">Guests</Heading>
-        {/* <QueryOperations filterObj={filterObj} sortByObj={sortByObj} /> */}
+        <SearchBy />
       </Wrapper>
       <Wrapper>
         <GuestsTable />
