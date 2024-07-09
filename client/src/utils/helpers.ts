c5e5b710ce3a4feaf6db666697203963007f4481
adel <adeelguitoun@gmail.com>
2024-07-09 15:22:46 +0100

    Create currency and time formatting helpers

---
+++ client/src/utils/helpers.ts
@@ -0,0 +1,13 @@
+import { formatDistance } from 'date-fns';
+
+export const formatCurrency = (value: number) =>
+  new Intl.NumberFormat('en', { style: 'currency', currency: 'USD' }).format(
+    value,
+  );
+
+export const formatDistanceFromNow = (date: Date) =>
+  formatDistance(date, new Date(), {
+    addSuffix: true,
+  })
+    .replace('about ', '')
+    .replace('in', 'In');
