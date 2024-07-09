dab719da15b8942ce41437a979f696e6e4fc5deb
adel <adeelguitoun@gmail.com>
2024-07-09 15:24:03 +0100

    Create Booking table component

---
+++ client/src/components/bookings/BookingTable.tsx
@@ -0,0 +1,35 @@
+import { useReadAllBookings } from '../../hooks/bookings';
+import Pagination from '../Pagination';
+import Spinner from '../Spinner';
+import Table from '../Table';
+import BookingRow from './BookingRow';
+
+const BookingTable = () => {
+  const { bookings, page, pages, total, isBookingsLoading } =
+    useReadAllBookings();
+
+  if (isBookingsLoading) return <Spinner />;
+  return (
+    <Table columns="1.5fr 1.5fr 2.4fr 1fr 1fr 0.5fr">
+      <Table.Header>
+        <div>Room</div>
+        <div>Guest</div>
+        <div>Dates</div>
+        <div>Status</div>
+        <div>Amount</div>
+        <div></div>
+      </Table.Header>
+      <Table.Body>
+        {bookings?.map((booking) => (
+          <Table.Row key={booking._id}>
+            <BookingRow booking={booking} />
+          </Table.Row>
+        ))}
+      </Table.Body>
+      <Table.Footer>
+        <Pagination pages={pages} page={page} total={total} />
+      </Table.Footer>
+    </Table>
+  );
+};
+export default BookingTable;
