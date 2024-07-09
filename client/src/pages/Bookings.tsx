112286fece856d6697a01318e8a4b19156e11dba
adel <adeelguitoun@gmail.com>
2024-07-09 15:23:47 +0100

    Add booking page structure

--- client/src/pages/Bookings.tsx
+++ client/src/pages/Bookings.tsx
@@ -1,4 +1,20 @@
+import Wrapper from '../components/ui/Wrapper';
+import Heading from '../components/ui/Heading';
+import BookingTable from '../components/bookings/BookingTable';
+import AddBooking from '../components/bookings/AddBooking';
+
 const Bookings = () => {
-  return <div>Bookings</div>;
+  return (
+    <>
+      <Wrapper direction="hr">
+        <Heading as="h1">Bookings</Heading>
+        {/* <QueryOperations filterObj={filterObj} sortByObj={sortByObj} /> */}
+      </Wrapper>
+      <Wrapper>
+        <BookingTable />
+        <AddBooking />
+      </Wrapper>
+    </>
+  );
 };
 export default Bookings;
