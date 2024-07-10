c62566bb121c19dfcedc5082ab333d6ddbd223cc
adel <adeelguitoun@gmail.com>
2024-07-10 17:24:01 +0100

    Create Add booking model component

--- client/src/components/bookings/AddBooking.tsx
+++ client/src/components/bookings/AddBooking.tsx
@@ -1,4 +1,19 @@
+import Modal from '../Modal';
+import Button from '../ui/Button';
+import BookingForm from './BookingForm';
+
 const AddBooking = () => {
-  return <div>AddBooking</div>;
+  return (
+    <div>
+      <Modal>
+        <Modal.Trigger name="add-guest">
+          <Button>Add Booking</Button>
+        </Modal.Trigger>
+        <Modal.Content name="add-guest">
+          <BookingForm />
+        </Modal.Content>
+      </Modal>
+    </div>
+  );
 };
 export default AddBooking;
