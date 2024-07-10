75b773185dcd592c6519cc5a2febcec69a065e67
adel <adeelguitoun@gmail.com>
2024-07-10 15:19:47 +0100

    Fix close the option menu when option is selected

--- client/src/components/AutoCompleteInput.tsx
+++ client/src/components/AutoCompleteInput.tsx
@@ -1,8 +1,9 @@
 import { useFormContext } from 'react-hook-form';
 import Input from './ui/Input';
-import { useState } from 'react';
-import styled, { css } from 'styled-components';
+import { createContext, useState } from 'react';
+import styled from 'styled-components';
 import { useSearchParams } from 'react-router-dom';
+import useClickOut from '../hooks/utils/useClickOut';
 
 export type Item = { id: string; name: string };
 
@@ -13,17 +14,20 @@ interface Props {
   disable?: boolean;
 }
 
+export const AutoCompleteInputContext = createContext<{
+  closeOption?: () => void;
+}>({});
+
 const StyledAutoCompleteInput = styled.div`
   position: relative;
 `;
 
-const StyledItemsList = styled.ul<{ danger?: string }>`
-  border: 1px solid var(--color-grey-300);
+const StyledItemsList = styled.ul`
+  border: 1px solid (--color-grey-300);
   border-radius: var(--border-radius-tiny);
   margin-top: 0.3rem;
   padding-inline: 0.3rem;
-  background-color: ${(props) =>
-    props.danger ? css`var(--color-red-100)` : css`var(--color-grey-0)`};
+  background-color: var(--color-grey-0);
 
   box-shadow: var(--shadow-md);
 
@@ -39,13 +43,22 @@ const StyledItemsList = styled.ul<{ danger?: string }>`
 
 const EmptyItem = styled.li`
   color: var(--color-red-700);
+  background-color: var(--color-red-100);
   padding: 1.5rem 1rem;
+  margin-inline: -0.5rem;
+  border-radius: var(--border-radius-tiny);
 `;
 
 const AutoCompleteInput = ({ name, items, render, disable }: Props) => {
   const { register } = useFormContext();
   const [searchParams, setSearchParams] = useSearchParams();
   const [isOpen, setIsOpen] = useState(false);
+  const closeOption = () => {
+    searchParams.delete(name);
+    setSearchParams(searchParams);
+    setIsOpen(false);
+  };
+  const ref = useClickOut(closeOption);
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (e.target.value) {
@@ -57,35 +70,32 @@ const AutoCompleteInput = ({ name, items, render, disable }: Props) => {
     }
   };
 
-  const handleBlur = () => {
-    setTimeout(() => setIsOpen(false), 200);
-    searchParams.delete(name);
-    setSearchParams(searchParams);
-  };
-
   return (
-    <StyledAutoCompleteInput>
-      <Input
-        type="text"
-        id={name}
-        {...register(`${name}-display`)}
-        placeholder={`Search for ${name}...`}
-        autoComplete="off"
-        disabled={disable}
-        onFocus={() => setIsOpen(true)}
-        onBlur={handleBlur}
-        onChange={handleChange}
-      />
-      <input type="hidden" {...register(name)} />
-      {items?.length === 0 && (
-        <StyledItemsList danger="danger">
-          <EmptyItem>No item found!</EmptyItem>
-        </StyledItemsList>
-      )}
-      {isOpen && items && (
-        <StyledItemsList>{items?.map(render)}</StyledItemsList>
-      )}
-    </StyledAutoCompleteInput>
+    <AutoCompleteInputContext.Provider value={{ closeOption }}>
+      <StyledAutoCompleteInput ref={ref}>
+        <Input
+          type="text"
+          id={name}
+          {...register(`${name}-display`)}
+          placeholder={`Search for ${name}...`}
+          autoComplete="off"
+          disabled={disable}
+          onChange={handleChange}
+          onMouseDown={() => setIsOpen(true)}
+        />
+        <input type="hidden" {...register(name)} />
+
+        {isOpen && (
+          <StyledItemsList>
+            {items?.length === 0 ? (
+              <EmptyItem>No item found!</EmptyItem>
+            ) : (
+              <>{items?.map(render)}</>
+            )}
+          </StyledItemsList>
+        )}
+      </StyledAutoCompleteInput>
+    </AutoCompleteInputContext.Provider>
   );
 };
 export default AutoCompleteInput;
