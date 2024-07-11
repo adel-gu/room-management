8b5977c335c41b164a16b49562c5d4c03d653039
adel <adeelguitoun@gmail.com>
2024-07-11 17:49:35 +0100

    Add booking page route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import AppLayout from './layouts/AppLayout';
 import Dashboard from './pages/Dashboard';
 import Bookings from './pages/Bookings';
+import Booking from './pages/Booking';
 import Rooms from './pages/Rooms';
 import Users from './pages/Users';
 import Settings from './pages/Settings';
@@ -14,6 +15,7 @@ const App = () => {
       <Route element={<AppLayout />}>
         <Route path="/" element={<Dashboard />} />
         <Route path="/bookings" element={<Bookings />} />
+        <Route path="/bookings/:bookingId" element={<Booking />} />
         <Route path="/guests" element={<Guests />} />
         <Route path="/rooms" element={<Rooms />} />
         <Route path="/users" element={<Users />} />
