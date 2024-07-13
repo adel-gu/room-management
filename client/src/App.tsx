1a9292c463afdd1e38a709f1af3ade7653566bdb
adel <adeelguitoun@gmail.com>
2024-07-13 17:43:01 +0100

    Add check-in route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,7 @@ import AppLayout from './layouts/AppLayout';
 import Dashboard from './pages/Dashboard';
 import Bookings from './pages/Bookings';
 import Booking from './pages/Booking';
+import Checkin from './pages/Checkin';
 import Rooms from './pages/Rooms';
 import Users from './pages/Users';
 import Settings from './pages/Settings';
@@ -16,6 +17,7 @@ const App = () => {
         <Route path="/" element={<Dashboard />} />
         <Route path="/bookings" element={<Bookings />} />
         <Route path="/bookings/:bookingId" element={<Booking />} />
+        <Route path="checkin/:bookingId" element={<Checkin />} />
         <Route path="/guests" element={<Guests />} />
         <Route path="/rooms" element={<Rooms />} />
         <Route path="/users" element={<Users />} />
