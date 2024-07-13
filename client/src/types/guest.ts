41a3432cd1f2e2e4b038c93190fd9e094f80203c
adel <adeelguitoun@gmail.com>
2024-07-13 18:54:34 +0100

    Update gust form schema

--- client/src/types/guest.ts
+++ client/src/types/guest.ts
@@ -12,27 +12,40 @@ export interface IGuest {
   email?: string;
 }
 
-export const formSchema = z.object({
-  fullName: z
-    .string({
-      required_error: 'required field',
-    })
-    .min(6, 'Must be at least 06 long character')
-    .trim(),
-  phone: z.string().refine(validator.isMobilePhone),
-  nationality: z
-    .string({
-      required_error: 'required field',
-    })
-    .trim(),
-  nationalID: z
-    .string({
-      required_error: 'required field',
-    })
-    .trim(),
-  status: z.nativeEnum(GuestStatus).optional().default(GuestStatus.Pending),
-  email: z.string().refine(validator.isEmail).optional(),
-});
+export const formSchema = z
+  .object({
+    fullName: z
+      .string({
+        required_error: 'required field',
+      })
+      .min(6, 'Must be at least 06 long character')
+      .trim(),
+    phone: z.string().refine(validator.isMobilePhone),
+    nationality: z
+      .string({
+        required_error: 'required field',
+      })
+      .trim(),
+    nationalID: z
+      .string({
+        required_error: 'required field',
+      })
+      .trim(),
+    status: z.nativeEnum(GuestStatus).optional().default(GuestStatus.Pending),
+    email: z.string().optional(),
+  })
+  .refine(
+    (data) => {
+      if (!!data.email) {
+        return validator.isEmail;
+      }
+      return true;
+    },
+    {
+      message: 'Must be a valid email',
+      path: ['email'],
+    },
+  );
 
 export type GuestFormData = z.infer<typeof formSchema>;
 
