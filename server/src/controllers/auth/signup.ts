b673f6a4e6953f25e5a998e5567c291670e10664
adel <adeelguitoun@gmail.com>
2024-07-15 09:26:36 +0100

    Create signup controller handler

---
+++ server/src/controllers/auth/signup.ts
@@ -0,0 +1,22 @@
+import { NextFunction, Request, Response } from 'express';
+import Admin from '../../models/admin';
+import setToken from './setToken';
+import catchErrors from '../../utils/catchErrors';
+import mongoose from 'mongoose';
+
+const signup = catchErrors(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const { name, email, password, passwordConfirm } = req.body;
+    const admin = await Admin.create({
+      name,
+      email,
+      password,
+      passwordConfirm,
+      tenantId: new mongoose.Types.ObjectId(),
+    });
+
+    setToken(res, admin._id.toString(), 'user sign up successfully');
+  },
+);
+
+export default signup;
