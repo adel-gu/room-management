932ac8b6dbe16be7f76a0527a49c1fff14e33167
adel <adeelguitoun@gmail.com>
2024-07-15 09:30:46 +0100

    Create auth router

---
+++ server/src/routes/authApi.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+import auth from '../controllers/auth';
+
+const router = express.Router();
+
+router.route('/signup').post(auth.signup);
+router.route('/login').post(auth.login);
+router.route('/forgot-password').post(auth.forgotPassword);
+router.route('/reset-password/:token').patch(auth.resetPassword);
+router.route('/logout').post(auth.checkAuthToken, auth.logout);
+
+export default router;
