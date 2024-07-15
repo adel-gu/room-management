8460127b6fd79bd87d7403993746068e9bd8e466
adel <adeelguitoun@gmail.com>
2024-07-15 09:30:37 +0100

    Create auth controller

---
+++ server/src/controllers/auth/index.ts
@@ -0,0 +1,15 @@
+import signup from './signup';
+import login from './login';
+import forgotPassword from './forgotPassword';
+import resetPassword from './resetPassword';
+import checkAuthToken from './checkAuthToken';
+import logout from './logout';
+
+export default {
+  signup,
+  login,
+  forgotPassword,
+  resetPassword,
+  checkAuthToken,
+  logout,
+};
