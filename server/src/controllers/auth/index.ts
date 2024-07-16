ac6ca9d4ca5d0ee1acf3bfe26973019bdc484cfd
adel <adeelguitoun@gmail.com>
2024-07-16 12:10:43 +0100

    Include verifyAccount

--- server/src/controllers/auth/index.ts
+++ server/src/controllers/auth/index.ts
@@ -4,9 +4,11 @@ import forgotPassword from './forgotPassword';
 import resetPassword from './resetPassword';
 import checkAuthToken from './checkAuthToken';
 import logout from './logout';
+import verifyAccount from './verifyAccount';
 
 export default {
   signup,
+  verifyAccount,
   login,
   forgotPassword,
   resetPassword,
