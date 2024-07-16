423c41a5f5b29410f5f9a94d12f22d5470bf87d1
adel <adeelguitoun@gmail.com>
2024-07-16 16:24:52 +0100

    Use catchErrors

--- server/src/controllers/crud/update.ts
+++ server/src/controllers/crud/update.ts
@@ -1,49 +1,41 @@
-import { Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 import mongoose from 'mongoose';
 import { ModelsEnum } from '../../utils/constants';
 import uploadImg from '../../utils/uploadImage';
+import catchErrors from '../../utils/catchErrors';
+import AppErrorHandler from '../../utils/appErrorHandler';
 
-const updateDoc = (model: string) => async (req: Request, res: Response) => {
-  try {
+const updateDoc = (model: string) =>
+  catchErrors(async (req: Request, res: Response, next: NextFunction) => {
     const Model = mongoose.model(model);
-    // find document
-    const doc = await Model.findById(req.params.id);
+    const doc = await Model.findOne({
+      _id: req.params.id,
+      tenantId: req.tenantId,
+    });
 
     if (!doc)
-      return res.status(404).json({
-        status: 'fail',
-        message: 'Document with that ID is not found',
-      });
+      return next(
+        new AppErrorHandler('Document with that ID is not found', 404),
+      );
 
     for (const key in req.body) {
       doc[key] = req.body[key];
     }
 
-    // Validate doc
-    const validationErrors = doc.validateSync();
-    if (validationErrors) {
-      return res.status(400).json({
-        status: 'fail',
-        message: validationErrors,
-      });
-    }
-
     // Upload the file
-    if (model === ModelsEnum.Room && req.file) {
+    if (
+      [`${ModelsEnum.Room}`, `${ModelsEnum.Admin}`].includes(model) &&
+      req.file
+    ) {
       doc.image = await uploadImg(req.file);
     }
 
-    // Save after validation success
-    await doc.save();
+    await doc.save({ validateModifiedOnly: true });
 
     res.status(200).json({
       message: 'success',
       data: doc,
     });
-  } catch (error) {
-    console.log('ERROR 💥:', error);
-    res.status(500).json({ status: 'error', message: 'Server Error' });
-  }
-};
+  });
 
 export default updateDoc;
