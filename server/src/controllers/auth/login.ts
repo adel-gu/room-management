463b75b04363a29b594fd617c8f1d03f3c88827d
adel <adeelguitoun@gmail.com>
2024-07-16 12:08:33 +0100

    Check if the admin is verified

--- server/src/controllers/auth/login.ts
+++ server/src/controllers/auth/login.ts
@@ -8,12 +8,22 @@ const login = catchErrors(
   async (req: Request, res: Response, next: NextFunction) => {
     const { email, password } = req.body;
 
-    const admin = await Admin.findOne({ email }).select('+password +salt');
+    const admin = await Admin.findOne({ email }).select(
+      '+password +salt +isVerified',
+    );
     if (
       !admin ||
       !(await admin.checkIsPasswordCorrect(password, admin.password))
     )
       return next(new AppErrorHandler('Invalid credentials', 401));
+
+    if (!admin.isVerified)
+      return next(
+        new AppErrorHandler(
+          'Account is not verified. Please check your email for verification link.',
+          401,
+        ),
+      );
 
     setToken(res, admin._id.toString(), 'user logged in successfully');
   },
