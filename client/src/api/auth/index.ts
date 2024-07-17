22b3f4a6258f1221b650e16ea4f5708c3158e686
adel <adeelguitoun@gmail.com>
2024-07-17 17:54:25 +0100

    Include verify account request

--- client/src/api/auth/index.ts
+++ client/src/api/auth/index.ts
@@ -1,3 +1,5 @@
 export { loginRequest } from './login';
 export { validateAuthRequest } from './validateAuth';
 export { logoutRequest } from './logout';
+export { registerRequest } from './register';
+export { verifyAccountRequest } from './verifyAccount';
