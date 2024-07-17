b3b2da3130ed5286a0c4265e2cba486cae3a1bdd
adel <adeelguitoun@gmail.com>
2024-07-17 13:51:42 +0100

    Auth api requests index

--- client/src/api/auth/index.ts
+++ client/src/api/auth/index.ts
@@ -1,21 +1,2 @@
-import { LoginData } from '../../types/Admin';
-
-const API_BASE_URL = `${import.meta.env.VITE_API_BASE_URL}/login`;
-
-// Login
-
-export const loginRequest = async (data: LoginData) => {
-  console.log('DATA: ', data);
-  const res = await fetch(API_BASE_URL, {
-    method: 'POST',
-    credentials: 'include',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(data),
-  });
-
-  if (!res.ok) throw new Error('Error while login');
-
-  return await res.json();
-};
+export { loginRequest } from './login';
+export { validateAuthRequest } from './validateAuth';
