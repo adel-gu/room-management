5c2a2eb7ff25a90a0dc47589fa0c5216b531bc08
adel <adeelguitoun@gmail.com>
2024-07-17 17:56:52 +0100

    Update token verification generating

--- server/src/controllers/auth/verifyAccount.ts
+++ server/src/controllers/auth/verifyAccount.ts
@@ -8,23 +8,23 @@ import AppErrorHandler from '../../utils/appErrorHandler';
 
 const verifyAccount = catchErrors(
   async (req: Request, res: Response, next: NextFunction) => {
-    const token = req.query.token?.toString();
-
-    const hashedToken = crypto
-      .createHash('sha256')
-      .update(token ?? '')
-      .digest('hex');
+    const token = req.body.token?.toString();
 
     const admin = await Admin.findOne({
-      passwordToken: hashedToken,
-      passwordTokenExpires: { $gt: Date.now() },
+      verificationCode: token,
+      verificationCodeExpires: { $gt: Date.now() },
     }).select('+isVerified');
 
     if (!admin)
-      return next(new AppErrorHandler('Token is Invalid or has expired', 400));
+      return next(
+        new AppErrorHandler(
+          'Token is Invalid, has been used, or has expired',
+          400,
+        ),
+      );
 
-    admin.passwordToken = undefined;
-    admin.passwordTokenExpires = undefined;
+    admin.verificationCode = undefined;
+    admin.verificationCodeExpires = undefined;
     if (!admin.isVerified) admin.isVerified = true;
 
     await admin.save({ validateBeforeSave: false });
