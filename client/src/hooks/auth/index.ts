0c46a2e076811184df743fe907f72b5380173d86
adel <adeelguitoun@gmail.com>
2024-07-17 13:50:34 +0100

    create validate auth api request hook

--- client/src/hooks/auth/index.ts
+++ client/src/hooks/auth/index.ts
@@ -1,14 +1,33 @@
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 import toast from 'react-hot-toast';
-import { loginRequest } from '../../api/auth';
+import { loginRequest, validateAuthRequest } from '../../api/auth';
+import { useNavigate } from 'react-router-dom';
 
 export const useLogin = () => {
+  const navigate = useNavigate();
+  const queryClient = useQueryClient();
   const { mutate: login, isPending: isLoginLoading } = useMutation({
     mutationKey: ['login'],
     mutationFn: loginRequest,
-    onSuccess: () => toast.success('Admin logged in successfully'),
+    onSuccess: async () => {
+      toast.success('Admin logged in successfully');
+      await queryClient.invalidateQueries({ queryKey: ['validateAuth'] });
+      navigate('/');
+    },
     onError: (err) => toast.error(err.message),
   });
 
   return { login, isLoginLoading };
+};
+
+export const useValidateAuth = () => {
+  const { data: isAuthenticated = false, isLoading: isAuthLoading } = useQuery({
+    queryKey: ['validateAuth'],
+    queryFn: () => validateAuthRequest(),
+  });
+
+  return {
+    isAuthenticated,
+    isAuthLoading,
+  };
 };
