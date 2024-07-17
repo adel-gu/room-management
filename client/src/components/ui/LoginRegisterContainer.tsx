3ffac40d7deaae22aa1d64941f368183a8a80fdd
adel <adeelguitoun@gmail.com>
2024-07-17 17:19:20 +0100

    Create container UI for login and register forms

---
+++ client/src/components/ui/LoginRegisterContainer.tsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const LoginRegisterContainer = styled.div`
+  min-height: 100vh;
+  display: grid;
+  grid-template-columns: auto;
+  align-content: center;
+  justify-content: center;
+  gap: 3.2rem;
+  background-color: var(--color-grey-50);
+`;
+
+export default LoginRegisterContainer;
