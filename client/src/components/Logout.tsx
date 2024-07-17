ac9a9f5c4b187547f90c3158762020994ae448a0
adel <adeelguitoun@gmail.com>
2024-07-17 14:40:53 +0100

    Use the logout hook

--- client/src/components/Logout.tsx
+++ client/src/components/Logout.tsx
@@ -1,10 +1,13 @@
 import { LogOut } from 'lucide-react';
 import ButtonIcon from './ui/ButtonIcon';
+import { useLogout } from '../hooks/auth';
+import Spinner from './Spinner';
 
 const Logout = () => {
+  const { logout, isLogoutLoading } = useLogout();
   return (
-    <ButtonIcon>
-      <LogOut />
+    <ButtonIcon disabled={isLogoutLoading} onClick={() => logout()}>
+      {!isLogoutLoading ? <LogOut /> : <Spinner size="sm" />}
     </ButtonIcon>
   );
 };
