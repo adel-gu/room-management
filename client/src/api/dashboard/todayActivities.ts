83b89e56cca8b56f6673f2ebe07d55d60ca863ac
adel <adeelguitoun@gmail.com>
2024-07-18 19:34:35 +0100

    Create  today's activities request end point

---
+++ client/src/api/dashboard/todayActivities.ts
@@ -0,0 +1,16 @@
+import { ITodayActivity } from '../../types/dashboard';
+
+const API_BASE_URL = `${import.meta.env.VITE_API_BASE_URL}/today-activities`;
+
+export const readTodayActivitiesRequest = async (): Promise<
+  ITodayActivity[]
+> => {
+  const res = await fetch(API_BASE_URL);
+
+  if (!res.ok) throw new Error('Error while calculating stats');
+  const {
+    data: { guests },
+  } = await res.json();
+
+  return guests;
+};
