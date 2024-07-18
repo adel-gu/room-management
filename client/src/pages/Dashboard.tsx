448de345e0eb89f5d3f145464eb8c1f8b7e37ae4
adel <adeelguitoun@gmail.com>
2024-07-18 19:31:50 +0100

    Create dashboard page

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -1,4 +1,17 @@
+import DashboardLayout from '../components/dashboard/DashboardLayout';
+import Heading from '../components/ui/Heading';
+import Wrapper from '../components/ui/Wrapper';
+
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  return (
+    <>
+      <Wrapper>
+        <Heading as="h1">Dashboard</Heading>
+        {/* Filters */}
+      </Wrapper>
+
+      <DashboardLayout />
+    </>
+  );
 };
 export default Dashboard;
