7f8244337f4667fc5041299908e8156cbdc57506
adel <adeelguitoun@gmail.com>
2024-07-19 20:15:43 +0100

    Include stays nights handler

--- server/src/controllers/dashboard/index.ts
+++ server/src/controllers/dashboard/index.ts
@@ -1,7 +1,9 @@
 import getStats from './stats';
 import getTodayActivities from './todayActivities';
+import getStaysNights from './staysNights';
 
 export default {
   getStats,
   getTodayActivities,
+  getStaysNights,
 };
