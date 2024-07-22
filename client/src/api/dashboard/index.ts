bc7156d56e800e64a8ba7137be5f48679cc58547
adel <adeelguitoun@gmail.com>
2024-07-22 12:47:45 +0100

    Include readSalesRequest

--- client/src/api/dashboard/index.ts
+++ client/src/api/dashboard/index.ts
@@ -1,3 +1,4 @@
 export { readStatsRequest } from './stats';
 export { readTodayActivitiesRequest } from './todayActivities';
 export { readStaysNightsRequest } from './staysNights';
+export { readSalesRequest } from './sales';
