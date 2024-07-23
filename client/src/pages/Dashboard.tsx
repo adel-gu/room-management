4908581c93f3495b72c087345ff33b9bb10dcf86
adel <adeelguitoun@gmail.com>
2024-07-23 09:36:46 +0100

    Add filtering component

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -1,13 +1,23 @@
 import DashboardLayout from '../components/dashboard/DashboardLayout';
+import QueryOperations from '../components/QueryOperations';
 import Heading from '../components/ui/Heading';
 import Wrapper from '../components/ui/Wrapper';
+
+const filterObj = {
+  field: 'last',
+  options: [
+    { value: '7', label: 'Last 7 days' },
+    { value: `30`, label: 'Last 30 days' },
+    { value: `90`, label: 'Last 90 days' },
+  ],
+};
 
 const Dashboard = () => {
   return (
     <>
-      <Wrapper>
+      <Wrapper direction="hr">
         <Heading as="h1">Dashboard</Heading>
-        {/* Filters */}
+        <QueryOperations filterObj={filterObj} />
       </Wrapper>
 
       <DashboardLayout />
