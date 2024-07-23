a13aae142263155b671913cc0c951f426e3c8ab8
adel <adeelguitoun@gmail.com>
2024-07-23 09:37:40 +0100

    Add filter by 'last' field to the request query

--- client/src/api/dashboard/stats.ts
+++ client/src/api/dashboard/stats.ts
@@ -2,8 +2,10 @@ import { IStats } from '../../types/dashboard';
 
 const API_BASE_URL = `${import.meta.env.VITE_API_BASE_URL}/dashboard`;
 
-export const readStatsRequest = async (): Promise<IStats> => {
-  const res = await fetch(API_BASE_URL, { credentials: 'include' });
+export const readStatsRequest = async (query: string): Promise<IStats> => {
+  const res = await fetch(`${API_BASE_URL}?last=${query}`, {
+    credentials: 'include',
+  });
 
   if (!res.ok) throw new Error('Error while calculating stats');
   const { data } = await res.json();
--- client/src/api/dashboard/todayActivities.ts
+++ client/src/api/dashboard/todayActivities.ts
@@ -2,10 +2,12 @@ import { ITodayActivity } from '../../types/dashboard';
 
 const API_BASE_URL = `${import.meta.env.VITE_API_BASE_URL}/today-activities`;
 
-export const readTodayActivitiesRequest = async (): Promise<
-  ITodayActivity[]
-> => {
-  const res = await fetch(API_BASE_URL, { credentials: 'include' });
+export const readTodayActivitiesRequest = async (
+  query: string,
+): Promise<ITodayActivity[]> => {
+  const res = await fetch(`${API_BASE_URL}?last=${query}`, {
+    credentials: 'include',
+  });
 
   if (!res.ok) throw new Error("Error while calculating today's activities");
   const {
