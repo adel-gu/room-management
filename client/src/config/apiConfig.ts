6278fb1c334902945b07da7828e3c549f6a20073
adel <adeelguitoun@gmail.com>
2024-07-29 18:20:48 +0100

    Update base url

--- client/src/config/apiConfig.ts
+++ client/src/config/apiConfig.ts
@@ -1,4 +1,6 @@
-const base = import.meta.env.VITE_API_BASE_URL ?? '';
+const base = import.meta.env.VITE_API_BASE_URL ?? 'api/v1';
+
+console.log('BASE: ', base);
 
 export const setApiUrl = (endpoint: string) => {
   return base + `/${endpoint}`;
